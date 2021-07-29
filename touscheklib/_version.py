67942ad990a8499b27da153a5bc39a9c36a615da
Malte Titze <mtitze@users.noreply.github.com>
2021-07-29 15:23:01 +0200

    version control

--- touscheklib/_version.py
+++ touscheklib/_version.py
@@ -1,2 +1,5 @@
-import pkg_resources
-__version__ = pkg_resources.get_distribution('touscheklib').version
+with open('pyproject.toml', 'r') as f:
+    for line in f.readlines():
+        if 'version' in line:
+            __version__ = line.split('=')[1][:-1]
+            break

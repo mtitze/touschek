0d984a13323dbd4c426b2be289b0bc726e23b9fa
Malte Titze <mtitze@users.noreply.github.com>
2021-07-29 15:33:56 +0200

    version control p2

--- touscheklib/_version.py
+++ touscheklib/_version.py
@@ -1,4 +1,7 @@
-with open('pyproject.toml', 'r') as f:
+import os
+abspath = os.path.abspath('.')
+
+with open(f'{abspath}/pyproject.toml', 'r') as f:
     for line in f.readlines():
         if 'version' in line:
             __version__ = line.split('=')[1][:-1]
